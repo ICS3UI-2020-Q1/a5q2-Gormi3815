dddfaf8b1787351ebf655c802a7fa2ab885ae85a
Gormi3815 <gormi3815@wrdsb.ca>
2020-10-08 05:18:07 +0000

    counts to a positive integer

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
-/**
+import java.util.Scanner;
+/** this program will count to the positive integer provided by the user
  *
- * @author 
+ * @author Isaac
  */
 public class Main {
 
@@ -10,6 +11,12 @@ public class Main {
    */
   public static void main(String[] args) {
     // TODO code application logic here
-    
+    Scanner input = new Scanner(System.in);
+    System.out.println("Please enter a positive integer.");
+    int num = input.nextInt();
+    for (int count = 1; count < num + 1; count++){
+      System.out.print(count);
+
   }
+}
 }
